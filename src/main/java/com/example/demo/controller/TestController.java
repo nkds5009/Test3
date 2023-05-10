50c6e854374b36e92aac389df36cf41c997827e2
nkds5009 <nkds5009@gmail.com>
2023-05-10 15:21:35 +0900

    강의실에서 작업했단당

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TestController {
 	@GetMapping("/")
 	public String index() {
+		System.out.println("강의실에서 작업함");
 		return "index";
 	}
 	
